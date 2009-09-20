98f79aa7ffa15870195c9e2048fab9a966497130
Nick Joyce <nick@boxdesign.co.uk>
2009-09-20 21:33:38 +0100

    Clean up

--- setup.py
+++ setup.py
@@ -20,36 +20,38 @@ def get_install_requirements():
     return install_requires
 
 
-keyw = """\
+keyw = """ \
 amf amf0 amf3 flex flash remoting rpc http flashplayer air bytearray
 objectproxy arraycollection recordset actionscript"""
 
 readme = os.path.join(os.path.dirname(__file__), 'README.txt')
 
 
-setup(name = "plasma",
-      version = "0.0.1",
-      description = "Blaze DS clone in Python",
-      long_description = open(readme, 'rt').read(),
-      url = "http://plasmads.org",
-      author = "The Plasma Project",
-      install_requires = get_install_requirements(),
-      keywords = keyw,
-      packages = find_packages(exclude=["*.tests"]),
-      license = "MIT",
-      platforms = ["any"],
-      classifiers = [
-    "Development Status :: 1 - Planning",
-    "Natural Language :: English",
-    "Intended Audience :: Developers",
-    "Intended Audience :: Information Technology",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2.3",
-    "Programming Language :: Python :: 2.4",
-    "Programming Language :: Python :: 2.5",
-    "Programming Language :: Python :: 2.6",
-    "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-    "Topic :: Software Development :: Libraries :: Python Modules",]
+setup(
+    name='plasma',
+    version='0.0.1',
+    description='Blaze DS clone in Python',
+    long_description=open(readme, 'rt').read(),
+    url='http://plasmads.org',
+    author='The Plasma Project',
+    install_requires=get_install_requirements(),
+    keywords=keyw,
+    packages = find_packages(exclude=[''*.tests'']),
+    license = 'MIT',
+    platforms = ['any'],
+    classifiers = [
+        'Development Status :: 1 - Planning',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
