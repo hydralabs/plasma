ebce196f60e0cab5ff03a7b75f870d8c44566f0a
Nick Joyce <nick@boxdesign.co.uk>
2010-01-24 20:48:59 -0600

    Add tests for PagedMessage

--- plasma/test/flex/data/test_messages.py
+++ plasma/test/flex/data/test_messages.py
@@ -88,7 +88,6 @@ class DataMessageTestCase(unittest.TestCase):
         m = pyamf.decode(bytes, encoding=pyamf.AMF3).next()
 
         self.assertTrue(isinstance(m, messages.DataMessage))
-
         self.assertEquals(util.dict_for_slots(m), kwargs)
 
 
@@ -163,5 +162,88 @@ class SequencedMessageTestCase(unittest.TestCase):
         m = pyamf.decode(bytes, encoding=pyamf.AMF3).next()
 
         self.assertTrue(isinstance(m, messages.SequencedMessage))
+        self.assertEquals(util.dict_for_slots(m), kwargs)
+
+
+class PagedMessageTestCase(unittest.TestCase):
+    """
+    Tests for :class:`messages.PagedMessage`
+    """
+
+    def test_alias(self):
+        alias = pyamf.get_class_alias(messages.PagedMessage)
+
+        alias.compile()
+
+        self.assertTrue(alias.sealed)
+        self.assertFalse(alias.dynamic)
+
+        self.assertEquals(alias.static_attrs, ['body', 'clientId',
+            'correlationId', 'destination', 'headers', 'messageId',
+            'pageCount', 'pageIndex', 'sequenceId', 'sequenceSize',
+            'timeToLive', 'timestamp'])
+
+    def test_create(self):
+        m = messages.PagedMessage()
+
+        self.assertEquals(m.body, None)
+        self.assertEquals(m.timestamp, None)
+        self.assertEquals(m.destination, None)
+        self.assertEquals(m.clientId, None)
+        self.assertEquals(m.headers, {})
+        self.assertEquals(m.timeToLive, None)
+        self.assertEquals(m.messageId, None)
+        self.assertEquals(m.sequenceId, None)
+        self.assertEquals(m.sequenceSize, None)
+        self.assertEquals(m.pageCount, None)
+        self.assertEquals(m.pageIndex, None)
+
+        m = messages.PagedMessage(sequenceId='foo', sequenceSize='bar',
+            pageCount=3, pageIndex=123, spam='eggs')
+
+        self.assertFalse(hasattr(m, 'spam'))
+        self.assertEquals(m.body, None)
+        self.assertEquals(m.timestamp, None)
+        self.assertEquals(m.destination, None)
+        self.assertEquals(m.clientId, None)
+        self.assertEquals(m.headers, {})
+        self.assertEquals(m.timeToLive, None)
+        self.assertEquals(m.messageId, None)
+        self.assertEquals(m.sequenceId, 'foo')
+        self.assertEquals(m.sequenceSize, 'bar')
+        self.assertEquals(m.pageCount, 3)
+        self.assertEquals(m.pageIndex, 123)
+
+    def test_amf(self):
+        kwargs = {
+            'body': ['foo'],
+            'timestamp': 'bar',
+            'destination': 'baz',
+            'clientId': 'gak',
+            'headers': {'blarg': 'nah'},
+            'timeToLive': 103,
+            'messageId': 'spam',
+            'sequenceId': 'eggs',
+            'sequenceSize': 'blarg',
+            'correlationId': '1234',
+            'pageCount': 123,
+            'pageIndex': 3,
+        }
+
+        m = messages.PagedMessage(**kwargs)
+
+        bytes = pyamf.encode(m, encoding=pyamf.AMF3).getvalue()
+
+        self.assertEquals(bytes, '\n\x81C?flex.data.messages.PagedMessage'
+            '\tbody\x11clientId\x1bcorrelationId\x17destination\x0fheaders'
+            '\x13messageId\x13pageCount\x13pageIndex\x15sequenceId\x19'
+            'sequenceSize\x15timeToLive\x13timestamp\t\x03\x01\x06\x07foo'
+            '\x06\x07gak\x06\t1234\x06\x07baz\n\x0b\x01\x0bblarg\x06\x07nah'
+            '\x01\x06\tspam\x04{\x04\x03\x06\teggs\x06"\x04g\x06\x07bar')
+
+        m = pyamf.decode(bytes, encoding=pyamf.AMF3).next()
+
+        self.assertTrue(isinstance(m, messages.PagedMessage))
+        self.assertEquals(util.dict_for_slots(m), kwargs)
 
         self.assertEquals(util.dict_for_slots(m), kwargs)
