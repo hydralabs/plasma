8deda93da1babbfcbc993fe7e24289618a8e41d1
Thijs Triemstra <info@collab.nl>
2011-07-27 17:58:50 +0200

    fix issue with logo.

--- doc/conf.py
+++ doc/conf.py
@@ -140,10 +140,14 @@ html_title = '%s - %s' % (project, description)
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['html/static']
+#html_static_path = ['html/static']
 
 # The name of an image file (.ico) that is the favicon of the docs.
 #html_favicon = 'plasma.ico'
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = None
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
